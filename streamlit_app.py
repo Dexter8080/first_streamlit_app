b5c3a13d4d03f622e12217cace387a727bee39cd
Dexter8080 <136246292+Dexter8080@users.noreply.github.com>
2023-06-11 19:12:01 +0500

    Update streamlit_app.py
    Smart Index

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,8 +14,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #------------------------------------------------
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avacado','Stawberries'])
 # Display the table on the page.
-
 streamlit.dataframe(my_fruit_list)
