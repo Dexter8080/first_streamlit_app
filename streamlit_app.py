c5e00fc9376294758264ddb315798e1dce6b291a
Dexter8080 <136246292+Dexter8080@users.noreply.github.com>
2023-06-11 18:52:51 +0500

    Update streamlit_app.py
    Pandas and Pickup list

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,8 @@ streamlit.text('🐔 Hard-Boild Free-Range Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 #------------------------------------------------
+
+# Lets put a pic list here so they can pick the fruit they want to Include
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#display the table on the page 
 streamlit.dataframe(my_fruit_list)
