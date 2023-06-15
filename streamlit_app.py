c6b5940985294cebec72b4d1905682cca95e276d
Dexter8080 <136246292+Dexter8080@users.noreply.github.com>
2023-06-15 23:59:11 +0500

    Update streamlit_app.py
    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,4 +53,6 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('Thanks for adding', fruit_choice)
 #
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+#
+streamlit.write('---------------------------------------------')
 
