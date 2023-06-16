f7cdbccc67a44e37f2306981f591c382296e3c81
Dexter8080 <136246292+Dexter8080@users.noreply.github.com>
2023-06-16 17:47:55 +0500

    Update streamlit_app.py
    Error Fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ streamlit.header("The fruit load list contains:")
 #-----------------------------------------
 # Add a buttton to load the fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.dataframe(back_from_function)
